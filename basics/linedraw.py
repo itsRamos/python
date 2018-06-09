c18347fc98c75aa253e29e5e0ba00c4a2a68de78
Erick Ramos <33048492+itsRamos@users.noreply.github.com>
2018-06-09 01:53:18 -0700

    Create linedraw.py

---
+++ basics/linedraw.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 11 15:33:14 2018
+Modified May 31, 2018
+
+Note:  enumerate is a type, not a function, as
+it returns instances of the enumerate type
+
+@author: Kirby Urner
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+def makeline(m, b):
+    def line(x):
+        """line function"""
+        return m*x + b
+    return line
+
+domain = np.arange(-10, 10, 0.1)
+
+plt.figure()
+
+colors = ['red','orange','blue','green','black']
+for idx, slope in enumerate(range(-4, 1)):
+    f = makeline(slope, 10)
+    y = f(domain)
+    plt.plot(domain, y, color = colors[idx])
+
+colors = ['green', 'blue', 'orange', 'red', 'black']
+for idx, slope in enumerate(range(1, 5)):
+    f = makeline(slope, 10)
+    y = f(domain)
+    plt.plot(domain, y, color = colors[idx])
+    
+plt.xlabel("Domain")
+plt.title("Plotting Lines")
+plt.grid()
