71e846a742c8b2a77c5ef51ddb3040ea456d09af
Erick Ramos <33048492+itsRamos@users.noreply.github.com>
2019-06-04 22:11:44 -0700

    Update wordFinder.py

--- testExercises/wordFinder.py
+++ testExercises/wordFinder.py
@@ -1,28 +1,17 @@
 # Exercise to find a word from a given grid 
-
 BOARD = [
-    ['a','p','b','e'],
-    ['p','r','q','z'],
-    ['b','a','p','b']
+    ['h','i','h','e','l','l','o'],
+    ['p','e','q','z','h','e','l'],
+    ['y','h','h','i','o','l','l']
 ]
-# BOARD = [
-#     ['a','p','b','e'],
-#     ['c','r','q','z'],
-#     ['x','l','g','t']
-# ]
 
+# Variables to search in all directions
 x = [-1,-1,-1,0,0,1,1,1]
 y = [-1,0,1,-1,1,-1,0,1]
 
+# Variables to define the size of the board
 R = 3
-C = 4
-
-# This should come out as true
-WORD = 'apb' 
-# WORD = 'apbe' 
-
-# This should come false
-# WORD = 'ABTX'
+C = 7
 
 # Function to print elements of the given board
 def print_board_long(board):
@@ -30,24 +19,23 @@ def print_board_long(board):
     for board_row in range(n):
         board_element = board[board_row]
         k = len(board_element)
+        
         for board_col in range(k):
             print(board_element[board_col], end = ' ')
         print()
 
 # more optimized way to print the given board
 def print_board(board):
+    print("#BOARD#")
     for row in board:
         for elem in row:
             print(elem, end=' ')
         print()
 
-def search2D(board, row, col, word):
-    # if (board[row][col] != word[0]):
-    # if (board[row][col] is not word[0]):
+
+# Function to search for the adjacent letters
+def search_board(board, row, col, word):
     if (board[row][col] is not word[0]):
-        # print(board[row][col])
-        # print(word[0])
-        # print()
         return False
     
     for i in range(0,8):
@@ -55,46 +43,49 @@ def search2D(board, row, col, word):
         cd = col + y[i]
 
         for j in range(1, len(word)):
-            # print (j)
-            # print(word)
-            # print(len(word))
-            # print("rd = {},R = {},cd={}.c={}".format(rd,R,cd,C))
-            # print("rd = {},cd={}".format(rd,cd))
+            # If search goes out of bounds (edges)
             if (rd >= R or rd < 0 or cd >= C or cd < 0):
-                # print("Outta bounds")
                 break
             
             if (board[rd][cd] is not word[j]):
-                # print("board: {} didnt match word char: {}".format(board[rd][cd],word[j]))
                 break
 
             rd += x[i]
             cd += y[i]
-            # print("board:{} matched word: {}".format(board[rd][cd],word[j]))
-        # j + 1
+        
         if(j+1 is (len(word))):
-            # print("match found")
-            # print("j: {} matched len: {}".format(j, len(word)))
             return True
-    # return False
 
+
+# Function to search the board
 def pattern_search(board, word):
     for row in range(0,R):
         for col in range(0, C):
-            # print("in colummn: {}".format(col))
-            if(search2D(board, row, col, word)):
-                # print(search2D(board, row, col, word))
+            if(search_board(board, row, col, word)):
                 print("word '{}' found at {} , {}".format(word,row, col))
-                # print()
+
+
+# Function to print a single word
+def find_single_word(word):
+    print_board(BOARD)
+    pattern_search(BOARD, word)
+
+
+# Function to print multiple words
+def find_multiple_words(list_of_words):
+    print_board(BOARD)
+    n = len(list_of_words)
+    for i in range(n):
+        pattern_search(BOARD, list_of_words[i])
 
 
 def main():
-    # print_board(BOARD)
-    pattern_search(BOARD, WORD)
+    WORD = 'hi'
+    find_single_word(WORD)
 
-    #debugging
-    # print(search2D(BOARD,0,0,WORD))
-    # print(search2D(BOARD,1,1,WORD))
+    WORDS = ['hi','hello']
+    find_multiple_words(WORDS)
+ 
 
 if __name__ == '__main__':
     main()
